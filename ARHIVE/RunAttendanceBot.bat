ae0cc5cc81e57de09a33b31b4bdd62667256da01
Anikor <korina.drangoiQgmail.com>
2025-07-18 15:31:39 +0300

    chore: remove unused folders (ARHIVE, bot, public, Testing) and files (bot.php, index.php)

--- ARHIVE/RunAttendanceBot.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: Запускаем PowerShell с правами Админа и исполняем ваш PS1
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "Start-Process PowerShell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\xampp\htdocs\TG_Bot\run-bot.ps1""'"
